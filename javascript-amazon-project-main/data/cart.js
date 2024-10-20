3814320568d2360ebf91194e50f34458a5dcb02a
aditya <aspoonia2018@gmail.com>
2024-10-21 00:26:01 +0530

    save cart in local storage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,4 @@
-export let cart=[
+export let cart= JSON.parse(localStorage.getItem('cart')) || [
   {
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
@@ -8,6 +8,10 @@ export let cart=[
     quantity:1
 }
 ];
+
+function saveToStorage(){
+   localStorage.setItem('cart',JSON.stringify(cart));
+}
 
  export function addToCart(productID){
     let matchingItem;
@@ -25,6 +29,7 @@ export let cart=[
                   quantity:1
                  }) 
                }
+     saveToStorage();          
   }
   const newCart=[];
   export function removeFromCart(productID){
@@ -34,4 +39,5 @@ export let cart=[
           }
         })
         cart=newCart;
+        saveToStorage();
   }
