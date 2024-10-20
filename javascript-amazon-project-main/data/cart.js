283aa1b7690fc4fd1645ff24867e241513cd0c33
aditya <aspoonia2018@gmail.com>
2024-10-20 23:51:53 +0530

    delete products from cart on pressing delete

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,12 +1,12 @@
-export const cart=[
-    {
-        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity:2
-    },
-    {
-        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity:1
-    }
+export let cart=[
+  {
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2
+},
+{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1
+}
 ];
 
  export function addToCart(productID){
@@ -25,4 +25,13 @@ export const cart=[
                   quantity:1
                  }) 
                }
+  }
+  const newCart=[];
+  export function removeFromCart(productID){
+        cart.forEach((item)=>{
+          if(item.productId!=productID){
+            newCart.push(item);
+          }
+        })
+        cart=newCart;
   }
--- javascript-amazon-project-main/script/checkout.js
+++ javascript-amazon-project-main/script/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from'../data/cart.js';
+import {cart,removeFromCart} from'../data/cart.js';
 import { products } from '../data/products.js';
 import {currencyFormat} from'./utils/money.js';
 
@@ -36,10 +36,11 @@ cart.forEach((cartItem)=>{
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link"
+                         data-product-id=${matchingProduct.id}>
               Delete
             </span>
           </div>
@@ -93,9 +94,16 @@ cart.forEach((cartItem)=>{
 </div>`;
 
 });
-console.log(OrdersummaryHtml);
 
 document.querySelector('.js-order-summary').innerHTML=OrdersummaryHtml;
+
+document.querySelectorAll('.js-delete-link')
+                   .forEach((link)=>{
+                        link.addEventListener('click',()=>{
+                           const productID=link.dataset.productId;
+                           removeFromCart(productID);
+                        })
+                   });
 
 
 
